dd7a55553658e3c875321444ec616022ed3d8032
aditya <adityatandulkar20@gmail.com>
2023-04-22 14:22:01 +0530

    Initial commit

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,6 @@
 import tellZodiac from "./zodiac";
-
 import {combineReducers} from "redux";
+
 const rootReducer=combineReducers({
     tellZodiac
 });
--- src/store.js
+++ src/store.js
@@ -1,1 +1,6 @@
 //code here
+import { legacy_createStore as createStore } from "redux";
+import rootReducer from "./reducers/index";
+
+const store = createStore(rootReducer);
+export default store;
